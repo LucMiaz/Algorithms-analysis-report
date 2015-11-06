8af4adad3e681a292894ad0b12e3c419dd3b81ad
LucMiaz <lucmiaz@iMac-de-Luc.local>
2015-11-06 18:17:09 +0100

    Update

--- R/find_best.R
+++ R/find_best.R
@@ -1,4 +1,5 @@
 TPFP_func<-function(df, threshold){
+  
   TP<-tally(filter(df, spec>threshold & disc==1))
   FP<-tally(filter(df, spec>threshold & disc==0))
   totP<-tally(filter(df,disc==1))
@@ -39,9 +40,8 @@ find_best<-function(tff, authors=list(), qualities=list(),fixedthreshold=FALSE, 
       delta<-thresholds[[2]]-thresholds[[1]]
       for (i in thresholds){
           tpfp=TPFP_func(algtf, i)
-          #sums[nrow(sums)+1,]<- tpfp
-          sums<-rbind(sums, tpfp)
-          remove(dist_ax,d_ax, FPR,TPR)
+          sums[nrow(sums)+1,]<- tpfp
+          #sums<-rbind(sums, tpfp)
           
       }
       thresmax<-which.max(sums$dist_ax)
